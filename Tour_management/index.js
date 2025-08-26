903c8a16822b54df68c0cdff2e50e5aea61f9586
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2025-08-26 15:37:20 +0700

    Khởi tạo môi trường dự án

--- Tour_management/index.js
+++ Tour_management/index.js
@@ -1,1 +1,22 @@
-console.log("Okela");
+const express = require('express');
+const app = express();
+const path = require("path");
+const port = 3000;
+
+// Thiết lập views
+app.set('views', path.join(__dirname, "views"));
+app.set('view engine', 'pug');
+
+// Thiết lập thư mục chứa file tĩnh của Frontend
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/', (req, res) => {
+  res.render('admin/pages/test.pug', { 
+    title: 'Hey', 
+    message: 'Hello there!' 
+  })
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
