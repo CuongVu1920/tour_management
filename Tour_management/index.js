cbf9fd393b3b057052fcbb111159637100c5d28d
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2025-08-27 09:50:03 +0700

    Kết nối cơ sở dữ liệu

--- Tour_management/index.js
+++ Tour_management/index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const path = require("path");
 const port = 3000;
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://cuongvudev2911:Jrf3JdJlqwlKnr4h@cluster0.gdykxgc.mongodb.net/tour_management_2');
+
+const Tour = mongoose.model('Tour', 
+  { 
+    name: String,
+    vehicle: String,
+  })
+
 
 // Thiết lập views
 app.set('views', path.join(__dirname, "views"));
@@ -10,10 +19,14 @@ app.set('view engine', 'pug');
 // Thiết lập thư mục chứa file tĩnh của Frontend
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+
+  const tourList = await Tour.find({});
+  
   res.render('admin/pages/test.pug', { 
     title: 'Hey', 
-    message: 'Hello there!' 
+    message: 'Hello there!',
+    tourList: tourList
   })
 });
 
