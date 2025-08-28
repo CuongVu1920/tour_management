8197863a2bc369a21e8002f10bd6ceb620895440
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2025-08-28 10:24:29 +0700

    Cấu hình env

--- Tour_management/index.js
+++ Tour_management/index.js
@@ -1,16 +1,13 @@
 const express = require('express');
+require('dotenv').config()
 const app = express();
 const path = require("path");
 const port = 3000;
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://cuongvudev2911:Jrf3JdJlqwlKnr4h@cluster0.gdykxgc.mongodb.net/tour_management_2');
+mongoose.connect(process.env.DATABASE);
 
-const Tour = mongoose.model('Tour', 
-  { 
-    name: String,
-    vehicle: String,
-  })
-
+const tourController = require("./controllers/admin/tour.controller")
+const homeController = require("./controllers/client/home.controller");
 
 // Thiết lập views
 app.set('views', path.join(__dirname, "views"));
@@ -19,16 +16,9 @@ app.set('view engine', 'pug');
 // Thiết lập thư mục chứa file tĩnh của Frontend
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', async (req, res) => {
+app.get('/', homeController.home);
 
-  const tourList = await Tour.find({});
-  
-  res.render('admin/pages/test.pug', { 
-    title: 'Hey', 
-    message: 'Hello there!',
-    tourList: tourList
-  })
-});
+app.get('/tour', tourController.list);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
