951b7ea7e42986e219242ece2bb4ff0c3bd632e1
phanngocha <luuvu4869@gmail.com>
2025-08-30 13:04:05 +0700

    full setting

---
+++ front-end/assets/js/script.js
@@ -0,0 +1,130 @@
+
+// Filepond Image
+const listFilepondImage = document.querySelectorAll("[filepond-image]");
+let filePond = {};
+if(listFilepondImage.length > 0) {
+  listFilepondImage.forEach(filepondImage => {
+    FilePond.registerPlugin(FilePondPluginImagePreview);
+    FilePond.registerPlugin(FilePondPluginFileValidateType);
+    filePond[filepondImage.name] = FilePond.create(filepondImage, {
+      labelIdle: '+'
+    });
+  });
+}
+// End Filepond Imag
+
+// Category Create Form
+const categoryCreateForm = document.querySelector("#category-create-form");
+if(categoryCreateForm) {
+  const validation = new JustValidate('#category-create-form');
+
+  validation
+    .addField('#name', [
+      {
+        rule: 'required',
+        errorMessage: 'Vui lòng nhập tên danh mục!'
+      }
+    ])
+    .onSuccess((event) => {
+      const name = event.target.name.value;
+      const parent = event.target.parent.value;
+      const position = event.target.position.value;
+      const status = event.target.status.value;
+      const avatars = filePond.avatar.getFiles();
+      let avatar = null;
+      if(avatars.length > 0) {
+        avatar = avatars[0].file;
+      }
+      const description = tinymce.get("description").getContent();
+      
+      console.log(name);
+      console.log(parent);
+      console.log(position);
+      console.log(status);
+      console.log(avatar);
+      console.log(description);
+    })
+  ;
+}
+// End Category Create Form
+
+
+// Setting Website Info Form
+const settingWebsiteInfoForm = document.querySelector("#setting-website-info-form");
+if(settingWebsiteInfoForm) {
+  const validation = new JustValidate('#setting-website-info-form');
+
+  validation
+    .addField('#websiteName', [
+      {
+        rule: 'required',
+        errorMessage: 'Vui lòng nhập tên website!'
+      },
+    ])
+    .addField('#email', [
+      {
+        rule: 'email',
+        errorMessage: 'Email không đúng định dạng!',
+      },
+    ])
+    .onSuccess((event) => {
+      const websiteName = event.target.websiteName.value;
+      const phone = event.target.phone.value;
+      const email = event.target.email.value;
+      const address = event.target.address.value;
+      const logos = filePond.logo.getFiles();
+      let logo = null;
+      if(logos.length > 0) {
+        logo = logos[0].file;
+      }
+      const favicons = filePond.favicon.getFiles();
+      let favicon = null;
+      if(favicons.length > 0) {
+        favicon = favicons[0].file;
+      }
+
+      console.log(websiteName);
+      console.log(phone);
+      console.log(email);
+      console.log(address);
+      console.log(logo);
+      console.log(favicon);
+    })
+  ;
+}
+// End Setting Website Info Form
+
+
+// Setting Role Create Form
+const settingRoleCreateForm = document.querySelector("#setting-role-create-form");
+if(settingRoleCreateForm) {
+  const validation = new JustValidate('#setting-role-create-form');
+
+  validation
+    .addField('#name', [
+      {
+        rule: 'required',
+        errorMessage: 'Vui lòng nhập tên nhóm quyền!'
+      },
+    ])
+    .onSuccess((event) => {
+      const name = event.target.name.value;
+      const description = event.target.description.value;
+      const permissions = [];
+
+      // permissions
+      const listElementPermission = settingRoleCreateForm.querySelectorAll('input[name="permissions"]:checked');
+      listElementPermission.forEach(input => {
+        permissions.push(input.value);
+      });
+      // End permissions
+
+      console.log(name);
+      console.log(description);
+      console.log(permissions);
+    })
+  ;
+}
+// End Setting Role Create Form
+
+
